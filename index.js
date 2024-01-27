17c083fcf1bbe3892461496274a7516c758c6acb
sahosoftRajkishor <118872856+sahosoftRajkishor@users.noreply.github.com>
2024-01-27 17:04:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+    next(); // Important
+})
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 const PORT = process.env.PORT || 3001;
